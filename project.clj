5433487663d52f33b6080006720e33c3f3eab2f2
cursive code <cursivecode@gmail.com>
2014-03-09 11:08:28 -0400

    Update version

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject protractor-cljs "0.1.1"
+(defproject protractor-cljs "0.1.2"
   :description "Library for working with protractor"
   :url "http://github.com/cursivecode/protractor-cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[jasmine-cljs "0.1.3"]]
+  :dependencies [[jasmine-cljs "0.1.4"]]
 
   :min-lein-version "2.0.0"
   :source-paths ["src/clj"]
